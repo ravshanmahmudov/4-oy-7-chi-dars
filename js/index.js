111427906d3ac3b063b5177aa60ee7a04bd83aad
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-06-01 03:14:00 +1400

    masala

---
+++ js/index.js
@@ -0,0 +1,165 @@
+// 1chi masala
+
+// let a = +prompt("");
+// switch (a) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log("qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("kuz");
+//     break;
+//   default:
+//     console.log("bunday - fasil - yoq");
+//     break;
+// }
+
+// // 2chi masala
+// function ballHishoblash(){
+// let a = +prompt("");
+// switch(true){
+// case a>=90 && a<=100:
+//     alert("A")
+//     break;
+//     case a>=80 && a<90:
+//     alert("B")
+//     break;
+//     case a>=70 && a<80:
+//     alert("C")
+//     break;
+//     case a>=60 && a<70:
+//     alert("B")
+//     break;
+//     case a>=0 && a<60:
+//         alert("F")
+//         break;
+// default:
+//     console.log("BUNDAY BAL YOQ");
+//     break;
+// }
+// }
+// ballHishoblash()
+
+
+// 3chi masala
+
+// function vaqtKritish(){
+// let a = +prompt("vaqt kiriting");
+// switch(true){
+// case a>=12 && a<=17:
+//     alert("xayrli kun")
+//     break;
+// case a>17 && a<=21:
+//     alert("xayrli kech")
+//     break;
+// case a>21 && a<=24:
+// case a<0 && a<=5:
+//     alert("xayrli tun")
+//     break;
+// default:
+//     console.log("bunday vaqt yoq");
+//     break;
+// }
+// }
+// vaqtKritish()
+
+// 4chi masala
+
+// let raqam = prompt("raqam kiritig");
+// function togriRaqam(){
+// while(true){
+// if(raqam>0 && raqam<10 && raqam!==""){
+// alert(`siz kiritgan raqam ${raqam}`);
+// break;
+// }else{
+// raqam=prompt("raqam kiriting")
+// }
+// }
+
+// }
+// togriRaqam()
+
+
+// 5chi masala
+// let i =0;
+// while (i<=20){
+//     console.log(i);
+// i+=2
+// }
+
+// 6 chi masala
+
+// let soz =prompt("soz kiriting")
+// function TogriStop(){
+// while(true){
+//     if(soz.toLowerCase() == "stop"){
+//     alert("siz stop sozini kiritingiz")
+//     break;
+// }else{
+//     soz=prompt("soz kiriting")
+// }
+// }
+// }
+// TogriStop()
+// 6chi masala
+
+
+// let raqam = prompt("son kiriting");
+// function togriSon(){
+// while(true)
+// if(raqam/2){
+//     console.log(raqam);
+//     break;
+// }
+// }
+// togriSon()
+
+
+
+
+
+
+
+
+  
+// let j =1;
+// let k =1;
+// while (j<=30){
+// s=s*j;
+// j++
+// }
+// let a =1;
+// let b = 50;
+// for(let i=a; i<=b; a++){     
+
+// }
+
+// let a =+pr("");
+// let i =a;
+// let sum =0
+// while(i<=b){
+//     if(i%3 ==0 && i%5==0){
+//     sum+=i
+//     }
+//     i++
+// }
+
+// function test(arg1 = 1, arg2 = 2, arg3 = 3){
+// let s = arg1+arg2+arg3;
+// return s ;
+// }
+// let result = test(3,4,5);
+// console.log(result);
